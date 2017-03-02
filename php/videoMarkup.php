4951177afe1334b3dbabf476909bd8dea668c775
Florian Le Gall <floy.legall@gmail.com>
2017-03-02 15:48:43 +0100

    Video Markup Point virugle

--- php/videoMarkup.php
+++ php/videoMarkup.php
@@ -2,19 +2,18 @@
   $projects=json_decode(file_get_contents('../data/projects.json'));
   foreach ($projects as $p) {
     if ($p->title == $_POST['name']) {
-      # code...
+      echo('<aside>');
+        echo('<h1 class="title">'.$p->title.'</h1>');
+        echo('<h2 class="participants">');
+        foreach ($p->participants as $participant) {
+          echo ('<span>'.$participant.'</span>');
+        }
+        echo('</h2>');
+        echo ('<p class="description">'.$p->description.'</p>');
+        echo ('<button class="back">Retour</button>');
+      echo('</aside>');
+      echo('<video src="../data/videos/'.$p->lien.'"></video>');
     }
   }
-  echo($_POST['name']);
-  echo('<aside>');
-    echo('<h1 class="title">'.$p->title.'</h1>');
-    echo('<h2 class="participants">')
-    foreach ($p->participants as $participant) {
-      echo ('<span>'.$participant.'</span>');
-    }
-    echo('</h2>');
-    echo ('<p class="description">'.$p->description.'</p>');
-    echo ('<button class="back">Retour</button>');
-  echo('</aside>');
-  echo('<video src="../data/videos/'.$p->lien.'"></video>');
+
 ?>

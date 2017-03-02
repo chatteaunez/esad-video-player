1dec3dd54417e6d5c18c8ded0429faa3db58b184
Florian Le Gall <floy.legall@gmail.com>
2017-03-02 14:09:34 +0100

    Menu markup

--- index.php
+++ index.php
@@ -4,10 +4,19 @@
     <link rel="stylesheet" href="css/video.css">
   </head>
 
+<?php
+  $projects=json_decode(file_get_contents('data/projects.json'));
+?>
+
   <body>
 
-    <button onclick="getVideo('test')">test</button>                  
-
+    <section class="menu">
+      <?php
+        foreach ($projects as $p) {
+          echo('<a class="" onclick="getVideo(\''.$p->title.'\');" data-image="data/images/'.$p->image.'">'.$p->title.'</a>');
+        }
+      ?>
+    </section>
     <script src="js/script.js"></script>
   </body>
 </html>
